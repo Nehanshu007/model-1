f44167ed05761236dfb42612e019704dfea6ab23
Parikh Nehanshu <afirefist641@gmail.com>
2026-05-14 13:39:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-function getWeather() {
+async function getWeather() {
 
   let city = document.getElementById("city").value;
 
-  if(city === "") {
+  if (city === "") {
     alert("Please enter city name");
     return;
   }
 
-  // Dummy weather data
-  document.getElementById("cityName").innerText = city;
-  document.getElementById("temp").innerText = "28°C";
-  document.getElementById("condition").innerText = "Cloudy ☁";
+  // YOUR API KEY
+  const apiKey = "a2af7cd0cdfd256d4a719fba3dc4cdb3";
+
+  // API URL
+  const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+
+    // Update UI
+    document.getElementById("cityName").innerText = data.name;
+
+    document.getElementById("temp").innerText =
+      `${Math.round(data.main.temp)}°C`;
+
+    document.getElementById("condition").innerText =
+      data.weather[0].main;
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
 }
